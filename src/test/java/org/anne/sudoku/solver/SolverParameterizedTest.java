d42816550957236edf79a81131e5009a5839aad8
dapitch666 <dapitch666@gmail.com>
2025-03-02 15:52:02 +0100

    Adding parametrizedTest

---
+++ src/test/java/org/anne/sudoku/solver/SolverParameterizedTest.java
@@ -0,0 +1,25 @@
+package org.anne.sudoku.solver;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SolverParameterizedTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "85...24..72......9..4.........1.7..23.5...9...4...........8..7..17..........36.4., 859612437723854169164379528986147352375268914241593786432981675617425893598736241",
+            "..53.....8......2..7..1.5..4....53...1..7...6..32...8..6.5....9..4....3......97.., 145327698839654127672918543496185372218473956753296481367542819984761235521839764"
+    })
+    void testMainWithParameterizedInput(String input, String expectedSolution) {
+        Sudoku sudoku = new Sudoku(input);
+        if (Solver.solveSudoku(sudoku, 0)) {
+            String solution = Arrays.stream(sudoku.solution).mapToObj(String::valueOf).collect(Collectors.joining());
+            assertEquals(expectedSolution, solution);
+        }
+    }
+}
